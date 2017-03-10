80995dbecf21dc8082952983d89135219cd6f2be
Ulises Ojeda Ogando <ulises.ojeda.contractor@bbva.com>
2017-03-10 12:54:37 +0100

    (chore) add module config files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='dict_map',
+      version='0.2',
+      description='Map over a dictionary',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+      ],
+      keywords='map dictionary',
+      url='https://github.com/ulisesojeda/dictionary_map',
+      author='Ulises Ojeda',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
